4acd4ac0720a9332f55bb8d67795f3d02caa2f1f
Lilly-27 <ghcecilia1@gmail.com>
2022-04-29 18:04:54 -0500

    Prepping for Deployment

--- models/reviews-model.js
+++ models/reviews-model.js
@@ -10,7 +10,7 @@ const ReviewSchema = new mongoose.Schema({
     years: Number,
     gender: String,
     review: String,
-}, {timestamps: true});
+}, {timestamp: true});
 
 const Review = mongoose.model('lucid', ReviewSchema);
 
